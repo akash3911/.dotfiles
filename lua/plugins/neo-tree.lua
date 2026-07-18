3aff1b725115838e26edf463230bb780ffd58be9
Akash Jonnalagadda <jonnalagaddaakash777@gmail.com>
2026-07-19 00:34:39 +0530

    idk

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,7 +8,11 @@ return {
       "MunifTanjim/nui.nvim",
     },
     config = function()
-      require("neo-tree").setup({})
+      require("neo-tree").setup({
+       window = {
+          width = 20, -- Change this (default is around 40)
+        },   
+      })
 
       vim.keymap.set("n", "<leader>e", "<Cmd>Neotree filesystem toggle left<CR>", {
         desc = "Toggle Explorer",
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,59 +1,83 @@
 return {
-    'nvim-telescope/telescope.nvim',
+    "nvim-telescope/telescope.nvim",
     dependencies = {
-        'nvim-lua/plenary.nvim',
+        "nvim-lua/plenary.nvim",
         {
-            'nvim-telescope/telescope-fzf-native.nvim',
-            build = 'make',
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "make",
         },
     },
+
     opts = {
         defaults = {
             file_ignore_patterns = {
-                '%.pem',
-                'node_modules/.*',
-                'secret.d/.*'
+                "%.pem",
+                "node_modules/.*",
+                "secret.d/.*",
             },
-            layout_strategy = 'horizontal',
+            layout_strategy = "horizontal",
             layout_config = {
                 horizontal = {
-                    prompt_position = 'top',
+                    prompt_position = "top",
                     preview_width = 0.55,
                 },
                 vertical = {
-                    mirror = false
+                    mirror = false,
                 },
                 width = 0.95,
                 height = 0.90,
                 preview_cutoff = 0,
             },
-        }
+        },
+
+        pickers = {
+            colorscheme = {
+                enable_preview = true,
+            },
+        },
     },
+
     config = function(_, opts)
-        local telescope = require('telescope')
+        local telescope = require("telescope")
 
-        telescope.load_extension('notify')
         telescope.setup(opts)
+        pcall(telescope.load_extension, "notify")
+        pcall(telescope.load_extension, "fzf")
 
-        local options = { noremap = false, silent = true }
+        local builtin = require("telescope.builtin")
+        local options = { noremap = true, silent = true }
 
         -- Builtin
-        vim.keymap.set('n', '<leader>fg', '<CMD>lua require("telescope.builtin").git_files{}<CR>', options)
-        vim.keymap.set('n', '<leader>ff', '<CMD>lua require("telescope.builtin").find_files()<CR>',
-            options)
-        vim.keymap.set('n', '<leader>fl', '<CMD>lua require("telescope.builtin").live_grep()<CR>', options)
-        vim.keymap.set('n', '<leader>fb', '<CMD>lua require("telescope.builtin").buffers()<CR>', options)
-        vim.keymap.set('n', '<leader>fh', '<CMD>lua require("telescope.builtin").help_tags()<CR>', options)
-        vim.keymap.set('n', '<leader>fd', '<CMD>lua require("telescope.builtin").diagnostics()<CR>', options)
-        vim.keymap.set('n', '<leader>fr', '<CMD>lua require("telescope.builtin").registers()<CR>', options)
+        vim.keymap.set("n", "<leader>fg", builtin.git_files, options)
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, options)
+        vim.keymap.set("n", "<leader>fl", builtin.live_grep, options)
+        vim.keymap.set("n", "<leader>fb", builtin.buffers, options)
+        vim.keymap.set("n", "<leader>fh", builtin.help_tags, options)
+        vim.keymap.set("n", "<leader>fd", builtin.diagnostics, options)
+        vim.keymap.set("n", "<leader>fr", builtin.registers, options)
 
+        -- Theme picker with live preview
+vim.keymap.set("n", "<leader>th", function()
+    require("telescope.builtin").colorscheme({
+        enable_preview = true,
+        layout_strategy = "horizontal",
+        layout_config = {
+            anchor = "E",
+            width = 0.45,
+            height = 0.35,
+            preview_width = 0.55,
+        },
+    })
+end, { desc = "Theme Picker" })
         -- Language Servers
-        vim.keymap.set('n', '<leader>lsd', '<CMD>lua require("telescope.builtin").lsp_definitions{}<CR>', options)
-        vim.keymap.set('n', '<leader>lsi', '<CMD>lua require("telescope.builtin").lsp_implementations{}<CR>', options)
-        vim.keymap.set('n', '<leader>lsl', '<CMD>lua require("telescope.builtin").lsp_code_actions{}<CR>', options)
-        vim.keymap.set('n', '<leader>lst', '<CMD>lua require("telescope.builtin").lsp_type_definitions{}<CR>', options)
+        vim.keymap.set("n", "<leader>lsd", builtin.lsp_definitions, options)
+        vim.keymap.set("n", "<leader>lsi", builtin.lsp_implementations, options)
+        --vim.keymap.set("n", "<leader>lsl", builtin.lsp_code_actions, options)
+        vim.keymap.set("n", "<leader>lst", builtin.lsp_type_definitions, options)
 
         -- Extensions
-        vim.keymap.set('n', '<leader>fn', '<CMD>lua require("telescope").extensions.notify.notify()<CR>', options)
+        vim.keymap.set("n", "<leader>fn", function()
+            telescope.extensions.notify.notify()
+        end, options)
     end,
 }
